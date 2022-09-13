f5ac7fea9683d51e74833af8a80ff87e03359b36
Yiyun Qin <yiyun.qin@mths.ca>
2022-09-13 13:55:46 +0000

    strange hello world

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * By:      Yiyun Qin
+ * Version: 1.0
+ * Since:   2022-09-131
+ */
+
+console.log('Hello, World!')
+
+console.log('\nDone.')
